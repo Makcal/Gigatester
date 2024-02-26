58aba5d70507e6a1436782b534694f71d7fe2358
Maxim Fomin <fominmaxim3721@gmail.com>
2024-02-26 17:37:45 +0300

    fix: remove tests with 1x1 matrices for AGLA 2

--- tester/generators/agla2.py
+++ tester/generators/agla2.py
@@ -21,15 +21,15 @@ class GeneratorAgla2Task1(AbsGenerator):
     @staticmethod
     def generate() -> str:
         if random.randint(0, 1):
-            m2 = m1 = random.randint(1, 4)
-            n2 = n1 = random.randint(1, 4)
+            m2 = m1 = random.randint(2, 4)
+            n2 = n1 = random.randint(2, 4)
         else:
-            m1 = random.randint(1, 4)
-            n1 = random.randint(1, 4)
-            m2 = random.randint(1, 4)
-            n2 = random.randint(1, 4)
-        m3 = random.randint(1, 4)
-        n3 = random.randint(1, 4)
+            m1 = random.randint(2, 4)
+            n1 = random.randint(2, 4)
+            m2 = random.randint(2, 4)
+            n2 = random.randint(2, 4)
+        m3 = random.randint(2, 4)
+        n3 = random.randint(2, 4)
 
         return f"{m1}\n{n1}\n{gen_matrix(m1, n1)}{m2}\n{n2}\n{gen_matrix(m2, n2)}{m3}\n{n3}\n{gen_matrix(m3, n3)}"
 
@@ -38,11 +38,11 @@ class GeneratorAgla2Task2(AbsGenerator):
     @staticmethod
     def generate() -> str:
         if random.randint(0, 1):
-            n2 = n1 = random.randint(1, 4)
+            n2 = n1 = random.randint(2, 4)
         else:
-            n1 = random.randint(1, 4)
-            n2 = random.randint(1, 4)
-        n3 = random.randint(1, 4)
+            n1 = random.randint(2, 4)
+            n2 = random.randint(2, 4)
+        n3 = random.randint(2, 4)
 
         return f"{n1}\n{gen_matrix(n1, n1)}{n2}\n{gen_matrix(n2, n2)}{n3}\n{gen_matrix(n3, n3)}"
 
@@ -50,14 +50,14 @@ class GeneratorAgla2Task2(AbsGenerator):
 class GeneratorAgla2Task3(AbsGenerator):
     @staticmethod
     def generate() -> str:
-        n = random.randint(1, 4)
+        n = random.randint(2, 4)
         return f"{n}\n{gen_matrix(n, n)}"
 
 
 class GeneratorAgla2Task4(AbsGenerator):
     @staticmethod
     def generate() -> str:
-        n = random.randint(1, 4)
+        n = random.randint(2, 4)
         return f"{n}\n{gen_matrix(n, n, bool(random.randint(0, 1)))}"
 
 
@@ -82,7 +82,7 @@ class GeneratorAgla2Task5(AbsGenerator):
 class GeneratorAgla2Task6(AbsGenerator):
     @staticmethod
     def generate() -> str:
-        n = random.randint(1, 4)
+        n = random.randint(2, 4)
         return f"{n}\n{gen_matrix(n, n)}{n}\n{gen_matrix(1, n)}"
 
 
