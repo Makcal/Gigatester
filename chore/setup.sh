f94b47f5f6a7a401bf9533ea9f0e0638c5a71884
Maxim Fomin <fominmaxim3721@gmail.com>
2024-03-05 15:08:12 +0300

    fix: add docker installation

--- chore/setup.sh
+++ chore/setup.sh
@@ -17,6 +17,8 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker pull makcal3000/gigatester-webserver
 
 mkdir data queue reference webserver webserver/static
 cd tester
