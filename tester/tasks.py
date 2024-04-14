4e2c77cbe3f7110095eef2d938e3fc7579815723
Maxim Fomin <fominmaxim3721@gmail.com>
2024-04-14 12:49:48 +0300

    feat: reduce number of tests (SSAD 3)

--- tester/tasks.py
+++ tester/tasks.py
@@ -52,8 +52,8 @@ AGLA_task5 = Task(GeneratorAgla2Task5(), ('AGLA2_task5.cpp', TESTER_DICT['cpp17'
 AGLA_task6 = Task(GeneratorAgla2Task6(), ('AGLA2_task6.cpp', TESTER_DICT['cpp17']), AglaIgnoreNegativeZerosChecker(), 100, 50)
 AGLA_task7 = Task(GeneratorAgla2Task7(), ('AGLA2_task7.cpp', TESTER_DICT['cpp17']), AglaIgnoreNegativeZerosChecker(), 100, 50)
 
-SSAD_task2 = Task(GeneratorSSADTask2(), ('SSAD_assignment2.cpp', TESTER_DICT['cpp20']), ComparisonChecker(), 400, 90)
-SSAD_task3 = Task(GeneratorSSADTask3(), ('SSAD_assignment3.java', TESTER_DICT['java']), ComparisonChecker(), 300, 90)
+SSAD_task2 = Task(GeneratorSSADTask2(), ('SSAD_assignment2.cpp', TESTER_DICT['cpp20']), ComparisonChecker(), 300, 90)
+SSAD_task3 = Task(GeneratorSSADTask3(), ('SSAD_assignment3.java', TESTER_DICT['java']), ComparisonChecker(), 200, 100)
 
 TASK_DICT: dict[str, Task] = {
     'DSA_week3A': DSA_week3A,
